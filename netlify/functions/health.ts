1211334404dcbaba6fdf79829c20bf6e097d84dc
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-12-28 18:45:14 +0200

    add fanction health

---
+++ netlify/functions/health.ts
@@ -0,0 +1,14 @@
+import { Handler } from "@netlify/functions";
+
+const handler: Handler = async (event, context) => {
+  const { headers } = event;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      headers,
+    }),
+  };
+};
+
+export { handler };
